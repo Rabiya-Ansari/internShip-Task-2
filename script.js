c1a0c4de0ce9cc052dd08f88f693486af5c99097
Rabiya <rabiya@gmail.com>
2025-12-03 04:15:10 -0800

    .......

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let display = document.getElementById("display");
-let button = document.querySelectorAll("button");
+let buttons = document.querySelectorAll("button");
 
-button.forEach(btn => {
-btn.addEventListener('click', ()=>{
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        let value = btn.innerText;
 
-    if(btn.innerText === 'C'){
-        display.value ="";
-    }
-     else if(btn.innerText ==="="){
-        display.value = eval(display.value)
-     }
-     else{
-        display.value += btn.innerText
-     }
-})
-})
+      
+        if (value === "C") {
+            display.value = "0";
+        }
+
+       
+        else if (value === "+/-") {
+            display.value = String(-parseFloat(display.value));
+        }
+
+       
+        else if (value === "%") {
+            display.value = String(parseFloat(display.value) / 100);
+        }
+
+     
+        else if (value === "=") {
+            display.value = eval(display.value);
+        }
+
+  
+        else if (display.value === "0") {
+            display.value = value;
+        }
+
+      
+        else {
+            display.value += value;
+        }
+    });
+});
